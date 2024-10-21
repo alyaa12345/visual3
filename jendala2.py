c4d2ea41bdf346e13f933bceb87130633e04a24c
alyaa12345 <alyanormaida123@gmail.com>
2024-10-21 13:06:27 +0800

    -

--- jendala2.py
+++
@@ -1,4 +0,0 @@
-import PySimpleGUI as sg  
-window = sg.Window(title ="latihan pertama", layout = [[sg.Text("Selamat belajar PySimpleGUI Alya")]],size=(400,200))
-window()
-Window.close()
--- jendela1.py
+++
@@ -1,4 +0,0 @@
-import PySimpleGUI as sg  
-window = sg.Window(title ="latihan pertama", layout = [[]],size=(400,200))
-window()
-Window.close()
